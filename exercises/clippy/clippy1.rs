ca360f8e11cd38f75cbf3c6708e311653820774f
DevDreamer-J <zhang.wanji@qq.com>
2024-10-19 23:57:43 +0800

    修改clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -20,7 +20,6 @@ fn main() {
     let area = pi * f32::powi(radius, 2);
 
     println!(
-        "The area of a circle with radius {:.2} is {:.5}!",
-        radius, area
+        "The area of a circle with radius {radius:.2} is {area:.5}!",
     )
 }
--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -17,10 +17,11 @@ fn main() {
         -1, -2, -3,
         -4, -5, -6
     ];
-    println!("My array! Here it is: {:?}", my_arr);
+    println!("My array! Here it is: {my_arr:?}", );
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5];
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let  mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
+    println!("This Vec is empty, see? {my_empty_vec:?}" );
 
     let mut value_a = 45;
     let mut value_b = 66;
@@ -28,5 +29,5 @@ fn main() {
     // value_a = value_b;
     // value_b = value_a;
     std::mem::swap(&mut value_a, &mut value_b);
-    println!("value a: {}; value b: {}", value_a, value_b);
+    println!("value a: {value_a}; value b: {value_b}" );
 }
